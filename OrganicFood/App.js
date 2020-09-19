33af5e5392f1d0f628473cf6e0ea8f2fb2743901
DaianeBarizon <daiane.barizon@neki-it.com.br>
2020-09-19 12:58:41 -0300

    Explica classe

--- OrganicFood/App.js
+++ OrganicFood/App.js
@@ -2,6 +2,8 @@
 // Antigamente não podia utilizar componentes funcionais para manipular states
 // Com hooks podemos manipular states em um componente funcional
 // Menos verboso
+
+/*
 import React from 'react';
 import {Text} from 'react-native';
 
@@ -12,6 +14,7 @@ const App = () => {
 };
 
 export default App;
+*/
 
 /*
 import React from 'react'
@@ -28,19 +31,40 @@ export default function App() {
 
 // Componentes Classe
 
-/*
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, {Component} from 'react';
+import {Text, View, TextInput, FlatList} from 'react-native';
 
 export default class App extends Component {
+  state = {
+    courses: [
+      {id: '1', nome: '•React'},
+      {id: '2', nome: '•React Native'},
+      {id: '3', nome: '•Java'},
+      {id: '4', nome: '•Node'},
+    ],
+    myText: 'Escrever um texto',
+  };
   // a única obrigação do componente classe é retornar um elemento visual no render
   // sempre tem que renderizar um componente na tela
+
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> Hello World! </Text>
+        <TextInput
+          value={this.state.myText}
+          onChangeText={(text) => {
+            this.setState({myText: text});
+          }}
+        />
+        <Text> {this.state.myText} </Text>
+        <FlatList
+          data={this.state.courses}
+          renderItem={({item}) => <Text>{item.nome}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+        <Text list={this.state.courses} />
       </View>
-    )
+    );
   }
 }
-*/
